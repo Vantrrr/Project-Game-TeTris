5739b69bb0fb68d22d02dff8aef792bf95c24357
Dinh Van <trankyvan08@gmail.com>
2024-04-05 11:28:50 +0700

    Handle function new bricks that drop random ->done

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -23,33 +23,38 @@ export default class Game {
         this.board = new Board(this);
         this.brick = new Brick(0, this);
         this.board.drawBoard();
+        this.generateNewBrick();
         this.brick.draw();
         // this.brick.moveLeft();
         // this.brick.moveRight();
         // this.brick.moveDown();
         // this.brick.rotate();
-       document.addEventListener('keydown', (e: KeyboardEvent) => {
-        console.log({e});
-                switch (e.code) {
-                    case this.constants.KEY_CODES.LEFT:
-                        this.brick.moveLeft();
-                        break;
-                    case this.constants.KEY_CODES.RIGHT:
-                        this.brick.moveRight();
-                        break;
-                    case this.constants.KEY_CODES.UP:
-                        this.brick.rotate();
-                        break;
-                    case this.constants.KEY_CODES.DOWN:
-                        this.brick.moveDown();
-                        break;
+        document.addEventListener('keydown', (e: KeyboardEvent) => {
+            console.log({ e });
+            switch (e.code) {
+                case this.constants.KEY_CODES.LEFT:
+                    this.brick.moveLeft();
+                    break;
+                case this.constants.KEY_CODES.RIGHT:
+                    this.brick.moveRight();
+                    break;
+                case this.constants.KEY_CODES.UP:
+                    this.brick.rotate();
+                    break;
+                case this.constants.KEY_CODES.DOWN:
+                    this.brick.moveDown();
+                    break;
 
-                }
+            }
         });
     }
-    
+
     public getApp(): PIXI.Application {
         return this.app;
+    }
+
+    generateNewBrick() {
+        this.brick = new Brick(Math.floor(Math.random() * 10) % this.constants.BRICK_LAYOUT.length, this);
     }
 
 }
