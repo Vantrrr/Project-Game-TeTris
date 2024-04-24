1d53a2e4ac00b7668875559997befc4ed3c0efdc
Hung <Emailnguyenthanhhung@gmail.com>
2024-04-24 21:08:39 +0700

    update

--- src/core/sound.ts
+++ src/core/sound.ts
@@ -1,26 +1,29 @@
 
-
-
-
-
-    export function  rotateSound() {//âm thanh xoay khối gạch 
-        const audio = new Audio('../assets/audio/rotate.mp3');
-        audio.play();
-    }
-    export function fallFastSound() {//âm thanh khối gạch rơi nhanh 
-        const audio = new Audio('../assets/audio/movefastdown.wav');
-        audio.play();
-    }
-    export function  fallBlockSound() {//âm thanh khối gạch rơi xuống
-        const audio = new Audio('../assets/audio/263006__dermotte__giant-step-1.mp3');
-        audio.play();
-    }
-    export function playEatSound() {//âm thanh ăn điểm
-        const audio = new Audio('../assets/audio/258020__kodack__arcade-bleep-sound.mp3');
-        audio.play();
-    }
-     export function gameoversound() {//âm thanh khối gạch rơi xuống
+let isGameOverSoundPlaying = false;
+export function rotateSound() {//âm thanh xoay khối gạch 
+    const audio = new Audio('../assets/audio/rotate.mp3');
+    audio.play();
+}
+export function fallFastSound() {//âm thanh khối gạch rơi nhanh 
+    const audio = new Audio('../assets/audio/movefastdown.wav');
+    audio.play();
+}
+export function fallBlockSound() {//âm thanh khối gạch rơi xuống
+    const audio = new Audio('../assets/audio/263006__dermotte__giant-step-1.mp3');
+    audio.play();
+}
+export function playEatSound() {//âm thanh ăn điểm
+    const audio = new Audio('../assets/audio/258020__kodack__arcade-bleep-sound.mp3');
+    audio.play();
+}
+export function gameoversound() {
+    if (!isGameOverSoundPlaying) {
         const audio = new Audio('../assets/audio/gameover.mp3');
+        isGameOverSoundPlaying = true;
         audio.play();
+        // Khi âm thanh kết thúc, đặt lại cờ
+        audio.onended = () => {
+            isGameOverSoundPlaying = false;
+        };
     }
-   
+}
